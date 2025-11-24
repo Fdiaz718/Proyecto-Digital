32acfbe70b1c68da40e073a90aa5bcbce41d6ab3
dcastrogon-coder <dcastrogon@unal.edu.co>
2025-11-24 09:38:10 -0500

    Update peripheral_mult.v

--- Codigos Calculadora/perifericos/peripheral_mult.v
+++ Codigos Calculadora/perifericos/peripheral_mult.v
@@ -1,25 +1,26 @@
-// Wrapper del multiplicador 8x8 para FemtoRV32
+// peripheral_mult.v
+// Wrapper del multiplicador para conectarlo al SOC
 
-module peripheral_mult (
+
+module peripheral_mult(
     input wire clk,
     input wire reset,
-    input wire [15:0] d_in,   
-    input wire cs,             
+    input wire [15:0] d_in,   /
+    input wire cs,            
     input wire [4:0] addr,    
     input wire rd,            
-    input wire wr,             
-    output reg [31:0] d_out  
+    input wire wr,            
+    output reg [31:0] d_out   
 );
 
-    
+
     reg start;
-    wire busy;
-    wire done;
     reg [7:0] A, B;
     wire [15:0] P;
+    wire busy, done;
 
-   
-    mult_top mult (
+
+    mult_top mult1 (
         .clk(clk),
         .reset(reset),
         .start(start),
@@ -30,26 +31,27 @@ module peripheral_mult (
         .done(done)
     );
 
-    
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            start <= 0;
             A <= 0;
             B <= 0;
-            d_out <= 0;
-        end else if (cs) begin
-            if (wr) begin
-                A <= d_in[15:8];  
-                B <= d_in[7:0];   
-                start <= 1;
-            end else begin
-                start <= 0;
-            end
+            start <= 0;
+        end else if (cs & wr) begin
+            // asigno operandos desde la CPU
+            A <= d_in[7:0];
+            B <= d_in[15:8];
+            start <= 1;  
+        end else begin
+            start <= 0; 
+        end
+    end
 
-            
-            if (rd) begin
-                d_out <= {16'd0, P};
-            end
+
+    always @(*) begin
+        if (cs & rd) begin
+            d_out = {16'd0, P};
+        end else begin
+            d_out = 32'd0;
         end
     end
 
